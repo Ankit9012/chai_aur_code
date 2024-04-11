cff11e4b6b6d1bf77f30b834ed2730fd4f535eb1
Ankit9012 <ix.ankit9012@gmail.com>
2024-04-11 10:35:04 +0530

    11 April 2024 | Conversion Operation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score = 333
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+/**
+ * "33"=>33
+ * "33ab"=> NaN, type of return number
+ * true=> 1 , false =0 
+ * 
+ */
+
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/**
+ * 1=> true, 0=> false
+ * ""=>false
+ * "hitesh"=>true   
+ * 
+ */
+
+
+let someNumber=33
+let sringNumber=String(someNumber)
+console.log(sringNumber);
+console.log(typeof sringNumber);
+
+
